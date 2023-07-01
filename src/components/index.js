e197afbd3353c411129c9e0ea08e98e13df8396c
AdamWisnumurti <a.wsnmrt@gmail.com>
2023-07-01 11:30:59 +0700

    error component

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,15 @@
 import AccordionItem from './accordion-item';
 // import Alert from './Alert';
-import AutoComplete from './autoComplete';
+import AutoComplete from './auto-complete';
 import Button from './button';
-import CategoryRating from './categoryRating';
-import DetailModal from './detailModal';
+import CategoryRating from './category-rating';
+import DetailModal from './detail-modal';
 import Header from './header';
-import ImageCarousel from './imageCarousel';
+import ImageCarousel from './image-carousel';
 import Layout from './Layout';
-import PageNotFound from './NotFoundDialog';
+import PageNotFound from './not-found-dialog';
 import Search from './search';
-import TableList from './tableList';
+import TableList from './table-list';
 
 export {
   AccordionItem,
